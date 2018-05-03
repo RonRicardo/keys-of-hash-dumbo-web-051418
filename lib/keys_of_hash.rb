3f385e1cf7187efde1ea13a975f8dd0a7df597f0
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-03 18:59:14 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,7 +4,7 @@ class Hash
   def keys_of(*arguments)
     return self.collect do |key, value|
         if arguments.include?(value)
-          return key
+           key
         end
       end
   end

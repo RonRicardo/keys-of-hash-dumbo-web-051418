3d9645fe475029b53dba0a55a189468d16be5cb8
Roniece Ricardo <roniecericardo@gmail.com>
2018-05-03 19:13:03 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,10 +2,9 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-     self.collect do |key, value|
-        if arguments.include?(value)
-           key
-        end
-      end
+    array = self.collect do |key, value|
+        key if arguments.include?(value)
   end
-end
+  array.compact #remove nil
+ end
+end 
